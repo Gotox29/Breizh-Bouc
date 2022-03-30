24d764e7bebaedb421ce6071c7ac696b985408dd
Anthony <anthony.nizac@gmail.com>
2022-03-30 14:35:04 +0200

    recherche (#9)
    intégration modification d'Aymeric

--- Breizh Bouc/actualite.php
+++ Breizh Bouc/actualite.php
@@ -7,9 +7,21 @@
             ?>
             </div>
                 <?php
-            for ($i = 0; $i < 10; $i++) {
-                include __DIR__ . "/components/publication.php";
-            }
+            //for ($i = 0; $i < 10; $i++) {
+              //  include __DIR__ . "/components/publication.php";
+           //}
+$query= "SELECT texte,users.username FROM `publication` INNER JOIN users ON publication.user_id = users.id order by uid desc";
+$stmt = MysqlConnect::getInstance()->link->prepare($query);
+$stmt->execute();
+$stmt->bind_result($texte, $username);
+
+while ($stmt->fetch()) {
+
+include __DIR__ . "/components/publication.php";
+
+
+}
+
             ?>
         </div>
     </div>
